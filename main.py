cd979f94e2caeaa025a11c7f99dd936ba136a57c
Stephen Gaydash <smgaydash8403@eagle.fgcu.edu>
2022-03-02 17:23:34 -0500

    Started.

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
+# Start codebase
 
+import time
+
+def inputdatabase():
+    # print("start inputdatabase")
+
+def logicinput():
+    print("start logicinput")
+
+def logic():
+    # print("Logic start")
+
+def userinterface():
+    # print("userinterface start")
+    time.sleep(2)
+    print("Welcome to DatabasePush!")
+    time.sleep(1)
+    print("Please select an option.")
+    option1 = input("1 - Use default settings for database. 2 - Input custom settings.")
+    time.sleep(2)
+    if option1 == "1":
+        print("Option 1 Selected")
+    elif option1 == "2":
+        print("Option 2 Selected")
+    else:
+        print("Invalid selection. Try again.")
+        userinterface()
